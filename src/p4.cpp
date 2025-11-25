a8924831267473e959c44e466b42136f38d70ddf
Joseph <jojo980580@gmail.com>
2025-11-25 14:32:59 +0800

    add p4

--- src/p4.cpp
+++ src/p4.cpp
@@ -1,10 +1,33 @@
-// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
-
 #include <iostream>
 using namespace std;
 
+void show(int depth) {
+    if (depth <= 0) return;
+    cout << "|--";
+    show(depth - 1);
+}
+
+
+int fib(int n, int depth) {
+
+    if (depth > 0) show(depth);
+    cout << "SEARCH fib(" << n << ")\n";
+    int re;
+    if (n == 1 || n == 2) {
+        re = 1;
+    } else {
+        int a = fib(n - 1, depth + 1);
+        int b = fib(n - 2, depth + 1);
+        re = a + b;
+    }
+    if (depth > 0) show(depth);
+    cout << "GET fib(" << n << ") = " << re << "\n";
+    return re;
+}
+
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    cout << fib(n, 0) << endl;
     return 0;
 }

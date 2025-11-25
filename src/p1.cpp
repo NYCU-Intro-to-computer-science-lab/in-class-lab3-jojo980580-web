53b748eb0294d11af65ca3f516d5847e149aefec
Joseph <jojo980580@gmail.com>
2025-11-25 14:22:32 +0800

    add p1

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,24 @@
 #include <iostream>
 using namespace std;
 
+long long factorial(int n) {
+    if (n == 0 || n == 1) 
+    return 1;
+
+    return n * factorial(n - 1);
+} 
+
+long long sum(int n){
+    if (n == 0) 
+    return 0;
+   return sum(n - 1) + n;
+}
 int main() {
-    
+    int n;
+    cin >> n;
+    cout<< factorial(n) << endl;
+    cout<< sum(n);  
     
     return 0;
 }
+
